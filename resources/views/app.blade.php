75cf4cd265acb1faaa04e64235ff4d0456b337e7
Ahmed IBrahim <AhmedIBrahim1057@gmail.com>
2021-08-10 20:45:42 +0200

    update

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -14,6 +14,7 @@
 
         <!-- Mobile Metas -->
         <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, shrink-to-fit=no">
+		<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 
         <!-- Web Fonts  -->
         <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800%7COpen+Sans:400,700,800" rel="stylesheet" type="text/css">
@@ -52,7 +53,7 @@
         <!-- Head Libs -->
         <script src="{{ asset('FrontendFiles/vendor/modernizr/modernizr.min.js') }}"></script>
 
-		<link rel="stylesheet" href="{{ secure_asset('css/AdminLTE.min.css') }}">
+
 
     </head>
     <body class="alternative-font-4 loading-overlay-showing" data-plugin-page-transition data-loading-overlay data-plugin-options="{'hideDelay': 100}">
